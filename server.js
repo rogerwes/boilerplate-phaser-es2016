b0824f1f2ea32aeee170f7d0d5f89f7d2f50d752
Wesley <wesleyrogers09@gmail.com>
2016-12-01 21:49:54 -0600

    Adding a simple server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var index = '.\public\index.html';
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(host);
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+app.use(function (req, res, next) {
+    console.log('cant get something');
+    res.status(404).send('Sorry cant find that!');
+});
